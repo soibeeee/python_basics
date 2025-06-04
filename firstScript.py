b9ee284b755c2fefe9756e026e9f5c2cf2c628d8
soibee <soibee@proton.me>
2025-06-05 01:25:20 +0530

    Resolved merge conflicts

--- firstScript.py
+++ firstScript.py
@@ -1,4 +1,26 @@
-print("Hello World")
-if 3 > 1 :
-    print("We are checking")
-    print("Checking Done")
+#print("Hello World")
+## print("We are checking")
+   # print("Checking Done")
+##
+
+# n = 0
+# where = input("Go left or right? ")
+# while where == "right":
+#     n +=1 
+#     if n > 2 :
+#         print("You are out")
+#     where = input("Go left or right? ")
+# print("You are out! ")
+
+# n = 0
+# while n < 5:
+#     print(n)
+#     n = n+1 
+
+x = 4
+i = 1
+factorial = 1
+while i <= x:
+    factorial = factorial * i
+    i = i + 1
+print("Factorial of", x, "is", factorial)
